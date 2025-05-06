5f7399a54896449cd7a3c2230ed29c448aaff5aa
Sehktel <24711055+Sehktel@users.noreply.github.com>
2025-05-06 21:08:20 +0300

    MIT

--- c51cc/project.clj
+++ c51cc/project.clj
@@ -1,8 +1,8 @@
-(defproject c51cc "0.1.0-SNAPSHOT"
+(defproject c51cc "0.1.0"
   :description "Clojure C Compiler"
   :url "https://github.com/Sehktel/c51cc"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :main ^:skip-aot c51cc.core
   :target-path "target/%s"
